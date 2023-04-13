8067404c5f29172cf25f5985f5815d6dbcb42991
marcelp94 <marcel.panzer@wi.uni-potsdam.de>
2023-04-13 09:02:23 +0200

    data

--- configs/config.py
+++ configs/config.py
@@ -1,13 +1,13 @@
 """Configuration file for SimPy Simulation, environment and performance measures"""
 
 configuration = {
-    # "SETUP_FILE": "Layout_FAZI.txt",
-    # "SETUP_FILE": "Layout_Scenario_Paper_AI.txt",
-    # "SETUP_FILE": "Layout_Scenario_Paper_AI_operation.txt",
-    "SETUP_FILE": "Layout_Scenario_Paper_AI_operation_dynamic.txt",
-    # "SETUP_FILE": "Layout_Scenario_Paper_EDD.txt",
-    # "SETUP_FILE": "Layout_Scenario_Cell_0_train.txt",
-    "SIMULATION_RANGE": 21000,
+    # "SETUP_FILE": "2_Layout_Scenario_Paper_AI.txt",
+    # "SETUP_FILE": "2_Layout_Scenario_Paper_AI_operation.txt",
+    "SETUP_FILE": "2_Layout_Scenario_Paper_AI_light.txt",
+    # "SETUP_FILE": "2_Layout_Scenario_Paper_Heuristic.txt",
+    # "SETUP_FILE": "2_Layout_Scenario_Paper_EDD.txt",
+
+    "SIMULATION_RANGE": 10000,
     "SEED_MACHINE_INTERRUPTIONS": 29378374,
     "MACHINE_FAILURE_RATE": 4,
     "FAILURE_MINIMAL_LENGTH": 10,
@@ -18,13 +18,13 @@ configuration = {
     "MACHINE_TIME_RELEASE_ITEM": 1,
 
     "SEED_INCOMING_ORDERS": 2578695,
-    "NUMBER_OF_ORDERS":10000,
+    "NUMBER_OF_ORDERS":5000,
     "ORDER_MINIMAL_LENGTH": 0,
     "ORDER_MAXIMAL_LENGTH": 100,
     "SPREAD_ORDER_COMPLEXITY": 0.1,
     # "SPREAD_ORDER_Priority": 1,
 
-    "AGENT_SPEED": 15,
+    "AGENT_SPEED": 90,
     "TIME_FOR_ITEM_PICK_UP": 0.1,
     "TIME_FOR_ITEM_STORE": 0.1,
 
--- configs/models.py
+++ configs/models.py
@@ -12,9 +12,9 @@ import sys
 # sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from collections import deque
 import tensorflow as tf
-from keras.models import Sequential, load_model
+from keras.models import Sequential, load_model, Model
 from keras.optimizers import RMSprop
-from keras.layers import Dense, Dropout, Activation
+from keras.layers import Dense, Dropout, Activation, Lambda, Input
 os.environ['keras_backend'] = 'tensorflow'
 from keras.backend import backend as K
 from keras.callbacks import History 
@@ -34,7 +34,7 @@ class ReinforceAgent():
         self.hidden_layer_size_1 = 128
         self.target_update = 3
         self.discount_factor = 0.98 #previous: 0.999
-        self.learning_rate = 0.997 #previous: 0.999
+        self.learning_rate = 0.005 #previous: 0.999
         self.epsilon_min = 0.01
         self.epsilon = 1
         self.epsilon_decay = 0.997
@@ -50,8 +50,13 @@ class ReinforceAgent():
         t = time.localtime()
         self.timestamp = time.strftime('%Y-%m-%d_%H-%M', t)
 
-        self.model = self.buildModel()
-        self.target_model = self.buildModel()
+        self.dqn = False
+        self.transfer_weights = False
+        self.double_DQN = True
+        self.dueling_DQN = True
+
+        self.model = self.buildModel(self.dqn)
+        self.target_model = self.buildModel(self.dqn)
         self.path = pathlib.Path(__file__).parent.resolve()
 
         if self.operational_mode:
@@ -60,37 +65,75 @@ class ReinforceAgent():
                 print("Neural network model found for agent with ID: ", self.identifier)
                 time.sleep(1)
             except:
-                self.model = self.buildModel()
-                self.target_model = self.buildModel()
+                self.model = self.buildModel(self.dqn)
+                self.target_model = self.buildModel(self.dqn)
                 print("No network found for agent with ID: ", self.identifier) 
                 print("Operational mode is switched to training mode") 
                 self.operational_mode = False  
         else:
-            self.model = self.buildModel()
-            self.target_model = self.buildModel()
+            self.model = self.buildModel(self.dqn)
+            self.target_model = self.buildModel(self.dqn)
 
-    def buildModel(self):
-        model = Sequential()
-        dropout = 0.01
-        model.add(Dense(self.hidden_layer_size_1, input_shape=(self.state_size,), activation='relu', kernel_initializer='lecun_uniform'))
-        model.add(Dense(128, activation='relu', kernel_initializer='lecun_uniform'))
-        model.add(Dropout(dropout))
-        model.add(Dense(self.action_size, kernel_initializer='lecun_uniform'))
-        model.add(Activation('linear'))
-        model.compile(loss='mse', optimizer=RMSprop(learning_rate=self.learning_rate, rho=0.9, epsilon=1e-06))
+    def buildModel(self, dqn = False):
+        # define a plain DQN neural network structure as a feed forward neural network
+        if self.dqn == True:
+            print("Compiled DQN model")
+            model = Sequential()
+            dropout = 0.01
+
+            # add input, output and hidden layers
+            model.add(Dense(self.hidden_layer_size_1, input_shape=(self.state_size,), activation='relu', kernel_initializer='lecun_uniform'))
+            model.add(Dense(128, activation='relu', kernel_initializer='lecun_uniform'))
+            model.add(Dropout(dropout))
+            model.add(Dense(self.action_size, kernel_initializer='lecun_uniform'))
+            model.add(Activation('linear'))
+            model.compile(loss='mse', optimizer=RMSprop(learning_rate=self.learning_rate, rho=0.9, epsilon=1e-06))
+            
+        # define a dueling DQN neural network structure with an advantage and critic network
+        if self.dueling_DQN == True:
+            if self.double_DQN == True:
+                print("Compiled dueling double DQN model")
+            else: 
+                print("Compiled dueling DQN model")
+            input_states = Input(shape=(self.state_size,))
+
+            # Common hidden layers
+            x = Dense(self.hidden_layer_size_1, activation='relu', kernel_initializer='lecun_uniform')(input_states)
+            x = Dense(128, activation='relu', kernel_initializer='lecun_uniform')(x)
+            x = Dropout(0.01)(x)
+
+            # Value stream
+            value = Dense(64, activation='relu', kernel_initializer='lecun_uniform')(x)
+            value = Dense(1, kernel_initializer='lecun_uniform')(value)
+
+            # Advantage stream
+            advantage = Dense(64, activation='relu', kernel_initializer='lecun_uniform')(x)
+            advantage = Dense(self.action_size, kernel_initializer='lecun_uniform')(advantage)
+            advantage = Lambda(lambda a: a - tf.reduce_mean(a, axis=-1, keepdims=True))(advantage)
+
+            # Combine value and advantage streams
+            q_values = Lambda(lambda v_a: v_a[0] + v_a[1])([value, advantage])
+
+            model = Model(inputs=input_states, outputs=q_values)
+            model.compile(loss='mse', optimizer=RMSprop(learning_rate=self.learning_rate, rho=0.9, epsilon=1e-06))
         
-        transfer_weights = False
-        if transfer_weights and self.state_size == 150:
+        # transfer of neural network weights
+        if self.transfer_weights and self.state_size == 150:
             self.epsilon = 0.5
+            transferred_layers = 0
             similar_model = load_model("../models_saved/best_models/" + str(self.identifier))
-            for i, layer in enumerate(model.layers[:-1]):  # Excluding the last layer
+            print("Transferred network parameters")
+            for i, layer in enumerate(model.layers[:-2]):  # Excluding the last layer
                 if isinstance(layer, Dense):
                     layer.set_weights(similar_model.layers[i].get_weights())
-
-        return model        
-
+                    transferred_layers += 1
+                    if transferred_layers >= 2:
+                        break
+    
+        return model    
+            
     def trainModel(self, target):
-        now_0 = time.time()
+        now_0 = time.time()  # Record start time
 
         # with open('../models_saved/memories/memory_' + str(self.identifier) + '.txt', 'r', newline='', encoding='utf-8') as f:
         #     reader = csv.reader(f)
@@ -99,59 +142,87 @@ class ReinforceAgent():
         # mini_batch = random.sample(memory, self.batch_size)
         # mini_batch = random.sample(data, self.batch_size)
 
+         # Sample a mini-batch of experiences from the replay memory
         mini_batch = random.sample(self.memory, self.batch_size)
+
+        # Initialize input (states) and output (target Q-values) batches
         X_batch = np.empty((0, self.state_size), dtype=int)
         Y_batch = np.empty((0, self.action_size), dtype=int)
+
+        # Iterate through each experience in the mini-batch
         for i in range(self.batch_size):
             states = np.asarray(mini_batch[i][0])
             next_states = np.asarray(mini_batch[i][3])
             actions = np.asarray(mini_batch[i][1])
             rewards = np.asarray(mini_batch[i][2])
-            q_value = self.model.predict(states.reshape(1, len(states)), verbose = 0)
-            self.q_value = q_value
-            next_target = self.target_model.predict(next_states.reshape(1, len(next_states)), verbose = 0)
-            # K.clear_session()
-            
 
-            best_online_action = np.argmax(self.model.predict(next_states.reshape(1, len(next_states)), verbose = 0))
+            # Get the current Q-values for the given state
+            q_value = self.model.predict(states.reshape(1, len(states)), verbose=0)
+
+            # Get the target Q-values for the next state
+            next_target = self.target_model.predict(next_states.reshape(1, len(next_states)), verbose=0)
+
+            # Get the best action according to the online model
+            best_online_action = np.argmax(self.model.predict(next_states.reshape(1, len(next_states)), verbose=0))
+
+            # Calculate the target Q-value for the selected action (plain or double DQN)
             next_q_value = self.getQvalue(rewards, next_target, best_online_action)
 
+            # Add the current state to the input batch
             X_batch = np.append(X_batch, np.array([states.copy()]), axis=0)
+
+            # Add the updated Q-value to the output batch
             Y_sample = q_value.copy()
             Y_sample[0][actions] = next_q_value
             Y_batch = np.append(Y_batch, np.array([Y_sample[0]]), axis=0)
+
+            # Clear the TensorFlow session to avoid memory leaks
             tf.keras.backend.clear_session()
+
+        # Train the model using the input and output batches
         self.model.fit(X_batch, Y_batch, batch_size=self.batch_size, epochs=1, verbose=0)
-        
+
+        # Calculate and store the training time
         time_tracker.time_train_calc = time.time() - now_0
+
+        # Clear memory
         gc.collect()
         # K.clear_session()
 
     def getQvalue(self, reward, next_target, best_online_action):
-        return reward + self.discount_factor * np.amax(next_target) # standard Q-learning
-        # return reward + self.discount_factor * next_target[0][best_online_action] # Double Q-Learning
+        if self.dqn == True:
+            # Calculate the target Q-value using standard Q-learning
+            return reward + self.discount_factor * np.amax(next_target)
+        if self.double_DQN == True:
+            # Calculate the target Q-value using Double Q-learning
+            return reward + self.discount_factor * next_target[0][best_online_action]
 
     def get_dispatch_rule(self, state):
         self.global_step = self.global_step + 1
+
         if not self.operational_mode:
+            # Decay the epsilon value if it's greater than the minimum value
             if self.epsilon > self.epsilon_min:
                 self.epsilon = self.epsilon * self.epsilon_decay
-                
+
+            # Determine whether to take a random action based on the epsilon value
             epsilon_random = np.random.rand()
             if epsilon_random <= self.epsilon:
+                # Take a random action
                 action = random.randint(0, self.action_size - 1)
                 return action
-                # return random.randrange(self.action_size)
             else:
+                # Take the best action according to the current Q-values
                 state = np.array(state)
-                q_value = self.model.predict(state.reshape(1, len(state)), verbose = 0)
+                q_value = self.model.predict(state.reshape(1, len(state)), verbose=0)
                 self.q_value = q_value
                 action = np.argmax(self.q_value[0])
                 return action
-
+            
         elif self.operational_mode:
+            # In operational mode, always take the best action according to the current Q-values
             state = np.array(state)
-            q_value = self.model.predict(state.reshape(1, len(state)), verbose = 0)
+            q_value = self.model.predict(state.reshape(1, len(state)), verbose=0)
             self.q_value = q_value
             action = np.argmax(self.q_value[0])
             return action
@@ -181,7 +252,10 @@ class ReinforceAgent():
                     #     self.rewards_average_old = np.average(self.rewards)
                     #     self.rewards = []
                     #     print("Best_model in cell ", cell_id, " updated")
-                    self.model.save("../models_saved/all_models/" + str(self.identifier) + "_" + self.timestamp + "_" + str(self.train_step))
+                    if self.dueling_DQN:
+                        self.model.save("../models_saved/all_models/dueling_" + str(self.identifier) + "_" + self.timestamp + "_" + str(self.train_step))
+                    else:
+                        self.model.save("../models_saved/all_models/" + str(self.identifier) + "_" + self.timestamp + "_" + str(self.train_step))
                     print("model_saved")
                     # self.model.save("../models_saved/all_models/" + self.timestamp + "_" + 'cell.id-' + str(cell_id) +  '_' + str(self) + "_" + str(self.action_size) + "_" + str(self.state_size) + "_" + str(self.hidden_layer_size_1) + "_" + str(self.batch_size) + "_" + str(self.train_step))
 
